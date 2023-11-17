9691b9e56fa233cedff1de671e13bcb503d2b066
Laura Naderimehr <lauranaderimehr@gmail.com>
2023-11-17 08:58:14 +0100

    rails-watch-list

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "lists#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
+  resources :lists, only: :destroy
 end
